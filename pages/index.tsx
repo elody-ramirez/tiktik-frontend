39b3c0ac17d7f157e84248d422d85fb12ae95e7e
elody-ramirez <elody.ramirez@gmail.com>
2022-12-08 11:58:36 -0500

    added base no results component, completed video card component, needed to add source of image to next config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
+import axios from 'axios'
+import NoResults from "../components/NoResults"
+import VideoCard from "../components/VideoCard"
+import { Video } from "../types"
 
-const Home = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+
   return (
-    <div>
-      TikTik
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => {
+          return <VideoCard post={video} key={video._id} />
+        })
+      ) : (
+        <NoResults text={"No Videos"}/>
+      )}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
