ef1dcd9f6b14d9a7ef8eee2ac55d225f94217159
elody-ramirez <elody.ramirez@gmail.com>
2022-12-08 22:25:30 -0500

    fully implemented google login to login in with a google account, register/login the user with our server to the db with sanity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/legacy/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-// import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from "../utils"
+
+import useAuthStore from "../store/authStore"
 
 const Navbar = () => {
+  const user = false
+  const { userProfile, addUser } = useAuthStore()
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-grey-200 py-2 px-4">
       <Link href="/">
@@ -21,6 +27,22 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => 
+              createOrGetUser(response, addUser)
+            }
+            onError={() => {
+              console.log('Error')
+            }}
+          />
+        )}
+      </div>
     </div>
   )
 }
