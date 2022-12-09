15b00898ec18152a59a479efaf9f89b717cb3db0
elody-ramirez <elody.ramirez@gmail.com>
2022-12-09 00:11:46 -0500

    Updated the Link href

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -34,7 +34,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
       <div>
         <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
           <div className="md:w-16 md:h-16 w-10 h-10">
-            <Link href="">
+            <Link href="/">
               <>
               <Image 
                 width={62}
