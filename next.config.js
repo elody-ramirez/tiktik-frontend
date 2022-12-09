3ed37882f5c101db69d4f86594cdc038b01970cb
elody-ramirez <elody.ramirez@gmail.com>
2022-12-09 00:11:27 -0500

    Added the google domain so users so we can access the user's profile picture

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['static1.cbrimages.com'],
+		domains: [
+			"static1.cbrimages.com",
+			"lh3.googleusercontent.com"
+		],
 	},
 }
 
