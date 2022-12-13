2be3224aed3181c97e97e73afd2813110adc543b
elody-ramirez <elody.ramirez@gmail.com>
2022-12-13 12:11:14 -0500

    completed the like button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import { MdFavorite } from "react-icons/md"
 
-const LikeButton = () => {
+import useAuthStore from "../store/authStore"
+
+interface IProps {
+  handleLike: () => void,
+  handleDislike: () => void,
+  likes: any[]
+}
+
+const LikeButton = ({ likes, handleLike, handleDislike}: IProps) => {
+  const [liked, setLiked] = useState(false)
+  const { userProfile }: any = useAuthStore()
+  const filterLikes = likes?.filter((item) => item._ref === userProfile?._id)
+
+  useEffect(() => {
+    if(filterLikes?.length > 0) {
+      setLiked(true)
+    } else {
+      setLiked(false)
+    }
+  }, [filterLikes, likes])
+
   return (
-    <div>LikeButton</div>
+    <div className="flex gap-6">
+      <div className="mt-4 flex flex-col justify-center items-center cursor-pointer">
+        {liked ? (
+          <div className="bg-primary rounded-full p-2 md:p-4 text-[#F51997]" onClick={handleDislike}>
+            <MdFavorite className="text-lg md:text-2xl" />
+          </div>
+        ) : (
+          <div className="bg-primary rounded-full p-2" onClick={handleLike}>
+            <MdFavorite className="text-lg md:text-2xl" />
+          </div>
+        )}
+        <p className="text-md font-semibold">{likes?.length | 0}</p>
+      </div>
+    </div>
   )
 }
 
