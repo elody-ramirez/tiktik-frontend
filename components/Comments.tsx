1a4943122cc6bba22ee0f562268d59a8f33cd26b
elody-ramirez <elody.ramirez@gmail.com>
2022-12-13 12:12:52 -0500

    added initial imports I should be using

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from "next/legacy/image"
+import Link from "next/link"
+import { GoVerified } from "react-icons/go"
+
+import useAuthStore from "../store/authStore"
+import NoResults from "./NoResults"
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <div>
+      
+    </div>
   )
 }
 
